6098d9dd6b6d46fb91a0948b406314fa7b36e674
d.barsukova <d.barsukova@g.nsu.ru>
2023-10-01 16:01:18 +0700

    lab3

---
+++ 22214/d.barsukova/lab3/lab3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void outputID_workWithFile(char *name) {
+    printf("real id: %d, effective id: %d\n", getuid(), geteuid());
+
+    FILE *file = fopen(name, "r");
+
+    if (file == NULL) {
+        perror("error opening file");
+        return;
+    }
+    if (fclose(file) == EOF) {
+        perror("error closing file");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    outputID_workWithFile(argv[1]);
+
+    int setNewUID = seteuid(getuid());
+    if (setNewUID == -1) {
+        perror("failed to set new user ID\n");
+    }
+
+    outputID_workWithFile(argv[1]);
+    return 0;
+}
