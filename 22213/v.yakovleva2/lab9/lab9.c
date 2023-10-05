84c063874c53a77ec6fa8b6680320b5710537494
melarozz <melarozz18@gmail.com>
2023-10-05 12:03:39 +0700

    lab9

--- 22213/v.yakovleva2/lab9/lab9.c
+++ 22213/v.yakovleva2/lab9/lab9.c
@@ -5,12 +5,12 @@
 int main() {
     pid_t pid = fork();
     if (pid == -1) {
-         return 0;
+         exit(1);
     }
 
     if (pid == 0) {
         execlp("cat", "cat", "task2.c", NULL);
-        return 1;
+        exit(1);
     }
     else {
         if(wait(NULL) != -1) {
