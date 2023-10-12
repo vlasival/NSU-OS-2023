1ed3fb7f714175838c50a5dd6803b759991c9777
Kuklin Georgiy <egorka99966@gmail.com>
2023-10-12 09:24:23 +0700

    lab3 fix. srry

--- 22213/g.kuklin/lab3/main.c
+++ 22213/g.kuklin/lab3/main.c
@@ -3,19 +3,23 @@
 #include <ucred.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-void check_file(const char *file) {
-    FILE *file = fopen(file, O_RDONLY);
-    if (file == NULL) {
+void check_file(const char *filepath) {
+    int file = open(filepath, O_RDONLY);
+
+    if (file < 0) {
         perror("Failed to open file");
         return;
     }
-    if (fclose(file)) {
+    if (close(file)) {
         perror("Failed to close file");    
-    };
+    }
 }
 
-int main(int argc, char *argv[]) {
+int main() {
     const char *filename = "test_file.txt";
     printf("Effective uid: %d. Real uid: %d.\n", geteuid(), getuid());
 
