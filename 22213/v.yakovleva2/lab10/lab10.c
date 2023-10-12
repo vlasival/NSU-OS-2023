2ed6691cc17c0a0f5a6fdefbab0d98c029f6c5aa
melarozz <melarozz18@gmail.com>
2023-10-12 11:57:28 +0700

    lab10

--- 22213/v.yakovleva2/lab10/lab10.c
+++ 22213/v.yakovleva2/lab10/lab10.c
@@ -1,27 +1,34 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdio.h>
+
 int main(int argc, char *argv[]) {
     if (argc == 1) {
         return 1;
     }
+
     pid_t pid = fork();
     switch (pid) {
-         case -1: return 1;
-         case 0:
-         {
-             execvp(argv[1], argv + 1);
-             return 0;
-         }
-         default:
-         {
+        case -1: return 1;
+        case 0:
+        {
+            execvp(argv[1], argv + 1);
+            return 0;
+        }
+        default:
+        {
             int stat_loc;
-            wait(&stat_loc);
-            if (WIFEXITED(stat_loc)) {
+            siginfo_t info;
+            waitid(P_PID, pid, &info, WEXITED);
+
+            if (info.si_code == CLD_EXITED) {
                 printf("\nChild process finished with exit code %d\n",
-                       WEXITSTATUS(stat_loc));
+                       info.si_status);
             }
         }
     }
+
     return 0;
 }
+
+
