c1c8f90bb17e668ff4ea581b2aee854adf632196
Kirill Romanov <romanov.K48@gmail.com>
2023-10-19 12:17:11 +0700

    changed wait

--- 22213/k.romanov1/lab9/main.c
+++ 22213/k.romanov1/lab9/main.c
@@ -5,7 +5,8 @@
 #include <stdio.h>
 
 int main() {
-    int waitstatus, pid;
+    int waitstatus;
+    pid_t pid;
     
     if ((pid = fork()) == - 1) {
             perror("failed to fork");
@@ -18,8 +19,11 @@ int main() {
         perror("exec one failed"); 
         exit(1);
     }
-    
-    wait(&waitstatus);
+   
+    if (waitid(P_PID, pid, NULL, WEXITED) == -1) {
+        perror("failed to wait child process");
+        exit(1);
+    }
 
     printf("task is completed!\n");
     exit(0);
