7677a750e680e6b06bcc821f0d4d5b17a7ed06fa
Valeria <64836918+melarozz@users.noreply.github.com>
2023-10-26 11:49:54 +0700

    lab25

--- 22213/v.yakovleva2/lab25/lab25.c
+++ 22213/v.yakovleva2/lab25/lab25.c
@@ -6,37 +6,41 @@
 
 int main() {
     int fildes[2];
-    if (pipe(fildes) == -1) {
-        return 1;
+    if (pipe(fildes) == -1) { 
+        return 1; 
     }
 
     pid_t pid = fork();
-    if (pid == -1) {
-        return 1;
-    }
+    switch(pid){
+        case -1: return 1; 
 
-    if (pid == 0) {
-        close(fildes[1]);
+        case 0:
+            close(fildes[1]);
 
-        char c;
-        while (read(fildes[0], &c, 1) != 0) {
-            printf("%c", toupper(c));
+            char c;
+            while (read(fildes[0], &c, 1) != 0) {
+                printf("%c", toupper(c));
+            }
+            printf("\n");
+
+            close(fildes[0]);
+         
+        default:{
+            close(fildes[0]);
+            
+            char *text = "This is some text\0";
+            for (char *ptr = text; *ptr != 0; ptr++) {
+                write(fildes[1], ptr, 1);
+            }
+
+            close(fildes[1]);
+            int status;
+            if (waitpid(pid, &status, 0)==-1){
+                return 1;
+            } 
         }
-        printf("\n");
 
-        close(fildes[0]);
-    }
-    else {
-        close(fildes[0]);
-
-        char *text = "This iS SomE Text\0";
-        for (char *ptr = text; *ptr != 0; ptr++) {
-            write(fildes[1], ptr, 1);
-        }
-
-        close(fildes[1]);
-        wait(NULL);
+        return 0;
     }
 
-    return 0;
 }
