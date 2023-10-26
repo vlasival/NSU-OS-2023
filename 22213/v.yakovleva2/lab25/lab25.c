11996c987d60143d98bb06edd4d8a6af93985b65
melarozz <v.yakovleva2@g.nsu.ru>
2023-10-26 08:52:01 +0700

    lab25

---
+++ 22213/v.yakovleva2/lab25/lab25.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main() {
+    int fildes[2];
+    if (pipe(fildes) == -1) {
+        return 1;
+    }
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        return 1;
+    }
+
+    if (pid == 0) {
+        close(fildes[1]);
+
+        char c;
+        while (read(fildes[0], &c, 1) != 0) {
+            printf("%c", toupper(c));
+        }
+        printf("\n");
+
+        close(fildes[0]);
+    }
+    else {
+        close(fildes[0]);
+
+        char *text = "This is some text\0";
+        for (char *ptr = text; *ptr != 0; ptr++) {
+            write(fildes[1], ptr, 1);
+        }
+
+        close(fildes[1]);
+        wait(NULL);
+    }
+
+    return 0;
+}
