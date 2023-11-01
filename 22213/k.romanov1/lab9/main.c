b2e0f2ceeb78dca66b7a520666aac258754595a7
Kirill Romanov <romanov.K48@gmail.com>
2023-11-01 21:22:36 +0700

    remade switch

--- 22213/k.romanov1/lab9/main.c
+++ 22213/k.romanov1/lab9/main.c
@@ -7,29 +7,25 @@
 int main() {
     pid_t pid;
     int wstatus;
+    siginfo_t child_info;
 
     switch(pid = fork()) {
         case (-1):
             perror("failed to fork");
             exit(1);
-            break;
         case (0):
             if (execlp("cat", "cat", "largefile", (char*) 0) == -1)
                 perror("failed to run cat");
 
             perror("exec one failed"); 
             exit(1);
-            break;
         default:
-            break;
+            if (waitpid(pid, &wstatus, WUNTRACED | WCONTINUED) == -1) {
+                perror("failed to wait child process");
+                exit(1);
+            }
+            printf("task is completed!\n");
+            exit(0);
     }
-    siginfo_t child_info; 
-    if (waitpid(pid, &wstatus, WUNTRACED | WCONTINUED) == -1) {
-        perror("failed to wait child process");
-        exit(1);
-    }
-
-    printf("task is completed!\n");
-    exit(0);
 }
 
