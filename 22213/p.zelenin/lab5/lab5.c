0b677ee910b161259a1665b5c05108670e159731
Pavel Zelenin <murdock8@mail.ru>
2023-11-15 11:52:24 +0700

    fixes

--- 22213/p.zelenin/lab5/lab5.c
+++ 22213/p.zelenin/lab5/lab5.c
@@ -7,7 +7,8 @@
 #define BUF_LEN 20
 #define READBUF_LEN 20
 
-void makeLinesTable(int fd, off_t *count, int *count_n, off_t *buf_n) {
+void makeLinesTable(int fd, off_t *count, int *count_n, off_t *buf_n, int* maxLen) {
+    int len = 0;
     buf_n[0] = -1;
     char *readBuffer = (char *) malloc(sizeof(char) * READBUF_LEN);
     if (readBuffer == NULL) {
@@ -24,6 +25,11 @@ void makeLinesTable(int fd, off_t *count, int *count_n, off_t *buf_n) {
             }
             if (readBuffer[i] == '\n') {
                 buf_n[++*(count_n)] = *(count) + (off_t) i;
+                (*maxLen) = *(maxLen) < len ? len : *(maxLen);
+                len = 0;
+            }
+            else {
+                len++;
             }
         }
         *(count) += (off_t) readBytes;
@@ -39,7 +45,7 @@ void makeLinesTable(int fd, off_t *count, int *count_n, off_t *buf_n) {
 
 
 int main(int argc, char* argv[]) {
-    int fd, count_n = 0;
+    int fd, count_n = 0, maxLen = 0;
     off_t count = 0;
     int capacity = BUF_LEN;
     int line;
@@ -61,22 +67,24 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    makeLinesTable(fd, &count, &count_n, buf_n);
+    makeLinesTable(fd, &count, &count_n, buf_n, &maxLen);
 
-    if (capacity < count) {
-        buf = (char* ) realloc(buf, sizeof(char) * (count + 1));
+    if (capacity < maxLen) {
+        buf = (char* ) realloc(buf, sizeof(char) * (maxLen + 1));
         if (buf == NULL) {
             perror("realloc() caused an error (buf) ");
             exit(EXIT_FAILURE);
         }
     }
 
+    int c;
     while (1) {
         printf("Enter the line number - ");
 
-        if (scanf("%d", &line) != 1) {
-            perror("Could not read the line number ");
-            exit(EXIT_FAILURE);
+        if ((scanf("%d", &line) != 1) || ((c = getchar()) != '\n')) {
+            while ((c = getchar()) != '\n');
+            printf("Could not read the line number\n");
+            continue;
         }
         if (line >= count_n || line < 0) {
             printf("There's no such line\n");
@@ -91,7 +99,7 @@ int main(int argc, char* argv[]) {
             perror("lseek() caused an error trying to get the line-");
             exit(EXIT_FAILURE);
         }
-        if (read(fd, buf, len) != len) {
+        if (read(fd, buf, len) != (ssize_t)len) {
             perror("read() caused an error (line reading)");
             exit(EXIT_FAILURE);
         }
