bdf3023c74a50e1d2bf6e8c3923fd3de848e8952
NotWhaleee <60616064+NotWhaleee@users.noreply.github.com>
2023-11-23 11:37:00 +0700

    lab9.c

---
+++ 22213/n.kozorez/lab9/lab9.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <wait.h>
+
+int main() {
+    pid_t child = fork();
+    if (child == -1) {
+        perror("Failed to fork");
+        exit(1);
+    } 
+    if (child == 0) {
+        if (execl("/bin/cat", "cat", "bigboy.txt", NULL) == -1) {
+            perror("Failed to execute cat");
+            exit(1);
+        }
+    } else {
+        siginfo_t child_info;
+        printf("%d\n", child);
+        if (waitid(P_PID, child, &child_info, WEXITED) == -1) {
+            perror("Failed to wait on child"); 
+            exit(1);
+        }
+        printf("Cat ended.\n"); 
+    };
+
+    return 0;
+}
